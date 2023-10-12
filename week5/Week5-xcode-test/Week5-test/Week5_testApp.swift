bd7c0574e153be10ddbeffd1ed41eec23328917a
Minlync <93282772+Minlync@users.noreply.github.com>
2023-10-12 12:17:20 -0400

    week5 start over

---
+++ week5/Week5-xcode-test/Week5-test/Week5_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week5_testApp.swift
+//  Week5-test
+//
+//  Created by Minglin Chen on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Week5_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
